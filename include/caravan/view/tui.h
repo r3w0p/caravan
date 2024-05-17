2c13aa04acdddb7d9e9726b56fb6a1b2a67b9df6
r3w0p <rr33ww00pp@gmail.com>
2024-05-17 16:14:23 +0100

    .exe from clion not working but vs does... hmm...

---
+++ include/caravan/view/tui.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2022-2024 r3w0p
+// The following code can be redistributed and/or
+// modified under the terms of the GPL-3.0 License.
+
+#ifndef CARAVAN_VIEW_TUI_H
+#define CARAVAN_VIEW_TUI_H
+
+#include <string>
+#include "caravan/model/game.h"
+#include "caravan/user/user.h"
+
+class TUI {
+public:
+    explicit TUI();
+};
+
+#endif //CARAVAN_VIEW_TUI_H
